9696a1cab6791aa08e76a83d9919d8c401c7a379
DhiogoSoares <dhiogosoaresrs@gmail.com>
2020-05-05 22:01:01 -0300

    initial commit with bd and project base of spring, gradle, jpa, bd driver

---
+++ src/main/java/com/example/projectFicha/ProjectFichaApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectFicha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFichaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectFichaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projectFicha/ProjectFichaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectFicha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFichaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
