114245cf8e6eb31d69241919a594c2fc011a888c
DhiogoSoares <dhiogosoaresrs@gmail.com>
2020-05-09 16:40:06 -0300

    Controlador da entidade Grupo parcialmente atualizado e corrigido erro de Exception

--- src/main/java/com/example/projectFicha/controladores/UsuarioControlador.java
+++ src/main/java/com/example/projectFicha/controladores/UsuarioControlador.java
@@ -13,7 +13,4 @@ public class UsuarioControlador {
     @Autowired
     UsuarioRepositorio usuarioRepositorio;
 
-
-    @RequestMapping()
-    public
 }
--- src/main/java/com/example/projectFicha/repositorios/GrupoRepositorio.java
+++ src/main/java/com/example/projectFicha/repositorios/GrupoRepositorio.java
@@ -4,5 +4,8 @@ import com.example.projectFicha.entidades.Grupo;
 import com.example.projectFicha.entidades.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface GrupoRepositorio extends CrudRepository<Grupo, Integer> {
+    List<Grupo> Find();
 }
