e16c322ec95aed7a8fdc8cd5d5195556e643c5f1
Pauline <51655254+PaulineAngel@users.noreply.github.com>
2020-05-09 12:22:51 -0300

    repositorios alterados para modelo JPA Spring, iniciado controllers

--- src/main/java/com/example/projectFicha/repositorios/Ficha.java
+++ src/main/java/com/example/projectFicha/repositorios/Ficha.java
@@ -1,8 +1,7 @@
 package com.example.projectFicha.repositorios;
 
-import com.example.projectFicha.entidades.Atributo;
 import com.example.projectFicha.entidades.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AtributoRepositorio extends CrudRepository<Atributo, Integer> {
+public interface Ficha extends CrudRepository<Ficha, Integer> {
 }
--- src/main/java/com/example/projectFicha/repositorios/GrupoRepositorio.java
+++ src/main/java/com/example/projectFicha/repositorios/GrupoRepositorio.java
@@ -1,4 +1,8 @@
 package com.example.projectFicha.repositorios;
 
-public interface GrupoRepositorio {
+import com.example.projectFicha.entidades.Grupo;
+import com.example.projectFicha.entidades.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GrupoRepositorio extends CrudRepository<Grupo, Integer> {
 }
